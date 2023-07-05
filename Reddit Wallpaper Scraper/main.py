6b04720fe8a4d958f79cc085be0b246692989ad8
Xinini <laro7500@gmail.com>
2023-07-05 15:06:54 +0200

    Can now change background with ctypes

--- Reddit Wallpaper Scraper/main.py
+++ Reddit Wallpaper Scraper/main.py
@@ -1,1 +1,7 @@
-import os
+import ctypes
+SPI_SETDESKWALLPAPER = 20 
+path = "C:\\Users\\cnilo\\Documents\\Python Portfolio\\Reddit Wallpaper Scraper\\test2.png"
+ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path , 0)
+print(path)
+
+"""https://dev.to/matin/change-your-windows-background-by-running-a-python-script-281p"""
