70566e28dc1b0983bfdd3ff6f661def30213664a
Xinini <laro7500@gmail.com>
2023-07-11 16:58:37 +0200

    Testing Praw API

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # RedditWallScrap
+## TODO LIST
+- Make a background folder with max. 7 pictures
+- Dynamically change path based on filetype and name
+- Webscrape from reddit 
+- Start up on PC start and automate
--- Reddit Wallpaper Scraper/main.py
+++ Reddit Wallpaper Scraper/main.py
@@ -1,7 +1,33 @@
 import ctypes
+import os
+import config
+import praw
+
+#Identification from config.py
+CLIENT_ID = config.clientID
+CLIENT_SECRET = config.clientSecret
+USERNAME = config.username
+PASSWORD = config.password
+USER_AGENT = "RedditWallScraperz"
+
+reddit = praw.reddit(client_id = CLIENT_ID,
+                     client_secret = CLIENT_SECRET,
+                     username = USERNAME,
+                     password = PASSWORD,
+                     user_agent = USER_AGENT
+                     )
+
+
+
+
 SPI_SETDESKWALLPAPER = 20 
-path = "C:\\Users\\cnilo\\Documents\\Python Portfolio\\Reddit Wallpaper Scraper\\test2.png"
+
+fileName = "test2.png"
+#path = "test2.png"
+#path = "C:\\Users\\cnilo\\Documents\\Python Portfolio\\Reddit Wallpaper Scraper\\test1.jpg"
+#print(path)
+#path = os.getcwd().split(">")[0]
 ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path , 0)
-print(path)
+#print(path)
 
 """https://dev.to/matin/change-your-windows-background-by-running-a-python-script-281p"""
